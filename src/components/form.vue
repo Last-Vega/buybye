9f42b5ab986bfd1c14d0015221adc216d112f67c
YukiShindo <s1811348@coins.tsukuba.ac.jp>
2020-11-20 16:14:36 +0900

    add_website

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,45 +1,46 @@
 <template>
-<div id="form">
-  <h2>購入理由をエレベーターピッチ風に書いてみましょう!</h2>
-  {{evp_template.temp1}}
-  <textarea
-    class="tweetBox"
-    placeholder="購買動機を入力してね"
-    name="why"
-    maxlength="140"
-    required
-    v-model="tweetContent.tweetWhy"
-    v-on:keyup="GetTweet(value)"
-    cols="40"
-    rows="2">
-  </textarea>
-  {{evp_template.temp2}}<br>
-  {{evp_template.temp3}}
-  <textarea
-    class="tweetBox"
-    placeholder="どんな問題が解決できるか入力してね"
-    name="whatOrWhy"
-    maxlength="140"
-    required
-    v-model="tweetContent.tweetWhat"
-    v-on:keyup="GetTweet(value)"
-    cols="40"
-    rows="2">
-  </textarea>
-  {{evp_template.temp4}}
-  <textarea
-    class="tweetBox"
-    placeholder="どのようにして解決できそうか入力してね"
-    name="how"
-    maxlength="140"
-    required
-    v-model="tweetContent.tweetHow"
-    v-on:keyup="GetTweet(value)"
-    cols="40"
-    rows="2">
-  </textarea>
-  {{evp_template.temp5}}
-  <!--
+  <div id="form">
+    <h2>購入理由をエレベーターピッチ風に書いてみましょう!</h2>
+    {{evp_template.temp1}}
+    <textarea
+      class="tweetBox"
+      placeholder="購買動機を入力してね"
+      name="why"
+      maxlength="140"
+      required
+      v-model="tweetContent.tweetWhy"
+      v-on:keyup="GetTweet(value)"
+      cols="40"
+      rows="2"
+    ></textarea>
+    {{evp_template.temp2}}
+    <br />
+    {{evp_template.temp3}}
+    <textarea
+      class="tweetBox"
+      placeholder="どんな問題が解決できるか入力してね"
+      name="whatOrWhy"
+      maxlength="140"
+      required
+      v-model="tweetContent.tweetWhat"
+      v-on:keyup="GetTweet(value)"
+      cols="40"
+      rows="2"
+    ></textarea>
+    {{evp_template.temp4}}
+    <textarea
+      class="tweetBox"
+      placeholder="どのようにして解決できそうか入力してね"
+      name="how"
+      maxlength="140"
+      required
+      v-model="tweetContent.tweetHow"
+      v-on:keyup="GetTweet(value)"
+      cols="40"
+      rows="2"
+    ></textarea>
+    {{evp_template.temp5}}
+    <!--
   {{evp_template.temp6}}
   <select
     class="tweetBox"
@@ -51,32 +52,27 @@
       <option value="できません">できません</option>
   </select>
   {{evp_template.temp7}}
-  -->
-  <p class="error"> {{ validation.validateResult}} </p>
-  <div id="TWEET">
-    <a
-      class="button"
-
-      >
-      Tweet
-    </a>
+    -->
+    <p class="error">{{ validation.validateResult}}</p>
+    <div id="TWEET">
+      <a class="button">Tweet</a>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
 export default {
-  name: 'form',
+  name: "form",
   data: function() {
     return {
       evp_template: {
-          temp1: "この商品は、",
-          temp2: "を解決する事を目的に買います。",
-          temp3: "これがあると",
-          temp4: "を",
-          temp5: "して問題を解決できます。",
-          temp6: "これは、今の私の所持品では代替",
-          temp7: "。"
+        temp1: "この商品は、",
+        temp2: "を解決する事を目的に買います。",
+        temp3: "これがあると",
+        temp4: "を",
+        temp5: "して問題を解決できます。",
+        temp6: "これは、今の私の所持品では代替",
+        temp7: "。"
       },
       tweetContent: {
         tweetWhy: null,
@@ -86,62 +82,84 @@ export default {
       validation: {
         validateResult: ""
       }
-
-    }
+    };
   },
   methods: {
-    GetTweet(str,code){
-      if (this.tweetContent.tweetWhy.length + this.tweetContent.tweetWhat.length + this.tweetContent.tweetHow.length < 20){
+    GetTweet(str, code) {
+      if (
+        this.tweetContent.tweetWhy.length +
+          this.tweetContent.tweetWhat.length +
+          this.tweetContent.tweetHow.length <
+        20
+      ) {
         this.validation.validateResult = "20字以上入力してください";
-      }else if (this.tweetContent.tweetWhy.length + this.tweetContent.tweetWhat.length + this.tweetContent.tweetHow.length > 87) {
+      } else if (
+        this.tweetContent.tweetWhy.length +
+          this.tweetContent.tweetWhat.length +
+          this.tweetContent.tweetHow.length >
+        87
+      ) {
         this.validation.validateResult = "熱入りすぎだよ！";
-      }else{
-        this.validation.validateResult = "めっちゃいい理由！"
+      } else {
+        this.validation.validateResult = "めっちゃいい理由！";
         var target = document.getElementById("form");
         var evp_box = document.getElementsByClassName("tweetBox");
         console.log(evp_box);
         var queryStr = location.search;
-        var queryArr = queryStr.split('=');
+        var queryArr = queryStr.split("=");
         var url = encodeURI(queryArr[1]);
         var text_all = "";
-        var text_all = this.evp_template.temp1 + evp_box[0].value + this.evp_template.temp2 + this.evp_template.temp3 + evp_box[1].value
-                       + this.evp_template.temp4 + evp_box[2].value + this.evp_template.temp5 + url;
-        var input_data = text_all.replace(/\r?\n/g, '%0D%0A');
-        TWEET.innerHTML = '<a class="button" href="https://twitter.com/intent/tweet?hashtags=Goodbuy&text=' + input_data + '" target="_blank">Tweet</a>'
+        var text_all =
+          this.evp_template.temp1 +
+          evp_box[0].value +
+          this.evp_template.temp2 +
+          this.evp_template.temp3 +
+          evp_box[1].value +
+          this.evp_template.temp4 +
+          evp_box[2].value +
+          this.evp_template.temp5 +
+          url;
+        var input_data = text_all.replace(/\r?\n/g, "%0D%0A");
+        TWEET.innerHTML =
+          '<a class="button" href="https://twitter.com/intent/tweet?hashtags=Goodbuy&text=' +
+          input_data +
+          '" target="_blank">Tweet</a>';
       }
     }
   }
-}
+};
 </script>
 
 <style lang="scss">
 .tweetBox {
-    border: 2px solid #00aced ;
-    border-radius: 0.67em;
-    padding: 0.5em;
-    background-color: snow;
-    width: 50em;
-    height: 15px;
-    font-size: 1em;
-    line-height: 1.2;
+  border: 2px solid #00aced;
+  border-radius: 0.67em;
+  padding: 0.5em;
+  background-color: snow;
+  width: 50em;
+  height: 15px;
+  font-size: 1em;
+  line-height: 1.2;
 }
 #TWEET {
-  width:150px;
-  height:30px;
-  line-height:30px;
-  text-align:center;
+  width: 150px;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
 
   background-color: #55acee;
   border: 2px solid #55acee;
   border-radius: 20px;
   color: #fff;
-  padding:4px 32px;
-  -webkit-transition: all .3s;
-  transition: all .3s;
+  padding: 4px 32px;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
 }
 #TWEET:hover {
   background-color: #fff;
   color: #55acee;
 }
-.error { color: red; }
+.error {
+  color: red;
+}
 </style>
