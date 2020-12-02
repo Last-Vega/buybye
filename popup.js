c48e7a7f8f53594ae9fbf3b2fbbd60d126deb51d
yugo sato <s1811494@klis.tsukuba.ac.jp>
2020-12-02 14:07:27 +0900

    add browser-action to heroku form

--- popup.js
+++ popup.js
@@ -1,9 +1,14 @@
+var tabUrl = "";
+chrome.tabs.getSelected(tab=>{  // 現在のタブを取得
+    tabUrl = tab.url;    // tab.titleには現在開いているタブのページタイトルが、tab.urlにはURLが格納されている。
+
+});
 
 $(function () {
     console.log("popupスクリプト");
-    const htmlPath = './index.html?url=' + location.href;
-    const htmlURL = chrome.extension.getURL(htmlPath);
+    const htmlPath = 'https://bloodcurdling-cemetery-16965.herokuapp.com/will/' + tabUrl;
+    /*const htmlURL = chrome.extension.getURL(htmlPath);:*/
     $("#go-to-form").click(function(){
-        window.open(htmlURL);
+        window.open(htmlPath);
       });
 });
